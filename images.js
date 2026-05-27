208a8f4aec18a7bd9b0c687eeb6e2a097c3e5af1
aoharujikkouiinnkai <aoharujikkouiinnkai@gmail.com>
2026-05-28 01:44:19 +0900

    Create images.js

---
+++ images.js
@@ -0,0 +1,11 @@
+// イラスト素材の定義ファイル
+const CARD_IMAGES = {
+    // スタンプ台の背景画像
+    background: 'bg.png',
+
+    // スタンプ画像の種類
+    stamps: {
+        live: 'stamp_live.png',       // リアタイ用手書きスタンプ
+        archive: 'stamp_archive.png'  // アーカイブ用汎用スタンプ
+    }
+};
