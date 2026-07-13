e49f392f8b61067e56904b88e2e5cba941a90ef9
aoharujikkouiinnkai <aoharujikkouiinnkai@gmail.com>
2026-07-13 10:53:32 +0900

    Delete images.js

--- images.js
+++
@@ -1,9 +0,0 @@
-// images.js
-// 📁 images/ フォルダの中にある画像を読み込む設定です
-const IMAGE_LIST = {
-    cardBase: 'images/cardhaikei.png', 
-    stamps: {
-        live: 'images/stamp_live.png',       
-        archive: 'images/stamp_archive.png'  
-    }
-};
